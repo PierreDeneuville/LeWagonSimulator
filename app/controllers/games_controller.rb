d376b80bd35eda857d2bb50bc648292da0264e94
christopher tavares <christavares33@gmail.com>
2021-11-24 12:27:22 +0100

    update hour and daily, they finally work

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,12 +1,12 @@
 class GamesController < ApplicationController
   def update
     @game = Game.find(params[:id])
+    students = @game.students
     if @game.current_hour < 18
       @game.hour_update
       students.each(&:hour_update)
     elsif @game.current_hour == 18 && @game.daily_challenge.position != 20
       @game.daily_update
-      students = @game.students
       students.each(&:daily_update)
     else
       # TODO : score total
