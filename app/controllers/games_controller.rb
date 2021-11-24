9386cd77351e28383822f1d981fe519cd4eef396
cedric.J <cedric_pro@mailoo.org>
2021-11-24 12:33:34 +0100

    create Student Buddie Game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,8 +1,24 @@
 class GamesController < ApplicationController
+  before_action :set_game, only: [:show]
 
   def index
     @games = Game.all
   end
 
+  def create
+    @game = Game.new(user: current_user, score: 0, is_over: false, daily_challenge: DailyChallenge.first, current_hour: 9)
+    @game.save
 
+    redirect_to game_path(@game)
+  end
+
+  def show
+    raise
+  end
+
+  private
+
+  def set_game
+    @game = Game.find(params[:id])
+  end
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -6,4 +6,42 @@ class Game < ApplicationRecord
 
   validates :current_hour, numericality: { only_integer: true }
   validates :current_hour, inclusion: { in: 9..18 }
+  after_create :create_students
+
+ STUDENTS_NAME = ["ced", "fred", "julien", "julia", "shimon", "pierre", "christopher", "nathan", "nadia", "suzette", "romain", "buffy", "jeremy", "moussa", "melanie", "claudine"]
+
+  def create_students
+    exercise = DailyChallenge.first.exercises.first
+    students = []
+    STUDENTS_NAME.each do |name|
+      students << Student.create!(name: name, lives: 3, exercise: exercise, success_probability: exercise.success_probability, game: self )
+    end
+    create_buddies(students)
+  end
+
+  private
+
+  def create_buddies(students)
+    count = 1
+    8.times do
+
+      student1 = students.sample
+      students.delete(student1)
+
+      student2 = students.sample
+      students.delete(student2)
+
+      BuddyPair.create!(first_student: student1, second_student: student2, daily_challenge: DailyChallenge.first)
+
+      student1.classroom_position = count
+      student1.save
+      count += 1
+
+      student2.classroom_position = count
+      student2.save
+      count += 1
+    end
+
+  end
+
 end
