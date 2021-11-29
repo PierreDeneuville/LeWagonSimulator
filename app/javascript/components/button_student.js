471a3ed4e039c13089bf734f05f73f9356817000
christopher tavares <christavares33@gmail.com>
2021-11-29 18:34:06 +0100

    some changes

--- app/javascript/components/button_student.js
+++ app/javascript/components/button_student.js
@@ -5,16 +5,17 @@ const helpStudent = () => {
     const students = document.querySelectorAll('.studs');
     students.forEach((student) => {
       student.addEventListener('click', (e) => {
+        console.log('test')
         e.preventDefault();
         buttonHelp.innerHTML = '';
         if(student.dataset.isHelped == 'false' && student.dataset.canHelp == 'true') {
           buttonHelp.insertAdjacentHTML('beforeend',
-            `<a class= 'button-rpg btn-buddy' rel='nofollow' data-method='patch' href='/games/${student.dataset.gameId}/students/${student.dataset.studentId}?help=student'>help buddy</a>`
+            `<a class= 'button-rpg btn-buddy' rel='nofollow' data-method='patch' data-remote="true" href='/games/${student.dataset.gameId}/students/${student.dataset.studentId}?help=student'>help buddy</a>`
           )
         }
         if (student.dataset.teacherHelp == '' && student.dataset.isHelped == 'false' && student.dataset.canHelp == 'true') {
           buttonHelp.insertAdjacentHTML('beforeend',
-            `<a class= 'button-rpg btn-teacher' rel='nofollow' data-method='patch' href='/games/${student.dataset.gameId}/students/${student.dataset.studentId}?help=teacher'>teacher help</a>`
+            `<a class= 'button-rpg btn-teacher' rel='nofollow' data-method='patch' data-remote="true" href='/games/${student.dataset.gameId}/students/${student.dataset.studentId}?help=teacher'>teacher help</a>`
           )
         }
       })
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -34,6 +34,7 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   helpStudent();
-  gameShowAnimate();
   animationLeaving();
 });
+
+gameShowAnimate();
