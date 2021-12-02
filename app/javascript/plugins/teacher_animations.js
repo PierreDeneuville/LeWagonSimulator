3bffe92d2d3758289fa59454db3d749ffbc5ec02
christopher tavares <christavares33@gmail.com>
2021-12-02 09:53:03 +0100

    button disabled and icones of students being helped by teacher

--- app/javascript/plugins/teacher_animations.js
+++ app/javascript/plugins/teacher_animations.js
@@ -1,8 +1,13 @@
 const buttonTeacher = document.querySelector('.btn-teacher')
+const students = document.querySelectorAll('.studs');
 
 const teacherAnimate = () => {
-  if(buttonTeacher) {
-    console.log('hello')
+  if(students) {
+    buttonTeacher.addEventListener('click', (e) => {
+      e.preventDefault();
+      console.log('hello')
+
+    })
   }
 }
 
