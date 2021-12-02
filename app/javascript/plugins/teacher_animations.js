822c18bc76da983ff298dec64c6e39b2a5acbad4
christopher tavares <christavares33@gmail.com>
2021-12-02 11:49:06 +0100

    first movements

--- app/javascript/plugins/teacher_animations.js
+++ app/javascript/plugins/teacher_animations.js
@@ -1,13 +1,25 @@
-const buttonTeacher = document.querySelector('.btn-teacher')
-const students = document.querySelectorAll('.studs');
 
+const teacherMove = () => {
+  const miniCharacter = document.querySelector('.mini-character')
+  const teacher = document.querySelector('.main-character')
+  const number = parseInt(miniCharacter.dataset.profHelping, 10)
+  const student = document.querySelector(`.student-${number}`)
+  const width = teacher.getBoundingClientRect().width
+  const toTop = student.getBoundingClientRect().top
+  const toLeft = student.getBoundingClientRect().left - width
+  $(teacher).animate({
+    left: toLeft
+  }, 1500)
+  $(teacher).animate({
+    top: toTop
+  }, 1500)
+}
 const teacherAnimate = () => {
+  const students = document.querySelectorAll('.studs');
   if(students) {
-    buttonTeacher.addEventListener('click', (e) => {
-      e.preventDefault();
-      console.log('hello')
-
-    })
+    setTimeout(teacherMove, 500)
+    // if (miniCharacter.dataset.profHelping !== '') {
+    // }
   }
 }
 
